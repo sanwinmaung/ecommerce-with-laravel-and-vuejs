3a2d198df706f55ad1cac564d8289e70e39d6efa
sanwinmaung <sanwinmaung.dev@gmail.com>
2023-06-08 10:06:42 +0630

    Developing Login View in Backend

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import store from "./store";
+import router from "./router";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).use(store).use(router).mount("#app");
